469bcbfaaf587ca8f5496c47da628aed7d8bfc28
Nils Hoffmann <nilshoffmann@users.sf.net>
2012-02-12 20:30:27 +0000

    Added maxThreads parameter to CompletionServiceFactory for local execution.

--- maltcms-execution-spi/src/main/java/net/sf/maltcms/execution/spi/CompletionServiceFactory.java
+++ maltcms-execution-spi/src/main/java/net/sf/maltcms/execution/spi/CompletionServiceFactory.java
@@ -5,9 +5,9 @@
 package net.sf.maltcms.execution.spi;
 
 import java.io.Serializable;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import lombok.Data;
-import net.sf.maltcms.execution.api.ICompletionService;
 
 /**
  *
@@ -19,9 +19,10 @@ public class CompletionServiceFactory<T extends Serializable> {
     private long timeOut = 5;
     private TimeUnit timeUnit = TimeUnit.SECONDS;
     private boolean blockingWait = false;
+    private int maxThreads = 1;
     
     public MaltcmsCompletionService<T> createVMLocalCompletionService() {
-        MaltcmsCompletionService<T> mcs = new MaltcmsCompletionService<T>(null,
+        MaltcmsCompletionService<T> mcs = new MaltcmsCompletionService<T>(Executors.newFixedThreadPool(maxThreads),
                 timeOut, timeUnit, blockingWait);
         return mcs;
     }
